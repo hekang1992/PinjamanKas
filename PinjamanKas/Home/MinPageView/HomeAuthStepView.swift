26de4e160295d1cae73fe7a333817da53b271ac0
hekang1992 <hekang19921111@hotmail.com>
2026-02-02 21:37:40 +0800

    a

--- PinjamanKas/Home/MinPageView/HomeAuthStepView.swift
+++ PinjamanKas/Home/MinPageView/HomeAuthStepView.swift
@@ -13,127 +13,151 @@ class HomeAuthStepView: UIView {
     var type: String? {
         didSet {
             guard let type = type else { return }
-            switch type {
-            case "extreme1":
-                self.nameLabel.text = "0%\ntersisa"
-                self.nameLabel.snp.updateConstraints { make in
-                    make.left.equalToSuperview().offset(327 * 0)
-                }
-                
-            case "extreme3":
-                self.nameLabel.text = "25%\ntersisa"
-                self.nameLabel.snp.updateConstraints { make in
-                    make.left.equalToSuperview().offset(327 * 0.25 - 32)
-                }
-                
-            case "extreme4":
-                self.nameLabel.text = "50%\ntersisa"
-                self.nameLabel.snp.updateConstraints { make in
-                    make.left.equalToSuperview().offset(327 * 0.5 - 32)
-                }
-                
-            case "extreme5":
-                self.nameLabel.text = "75%\ntersisa"
-                self.nameLabel.snp.updateConstraints { make in
-                    make.left.equalToSuperview().offset(327 * 0.75 - 32)
-                }
-            case "":
-                self.nameLabel.text = "100%\ntersisa"
-                self.nameLabel.snp.updateConstraints { make in
-                    make.left.equalToSuperview().offset(327 * 1 - 32)
-                }
-                
-            default:
-                break
-            }
+            updateProgress(type: type)
         }
     }
     
+    private let dashLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.strokeColor = UIColor(hex: "#E8FA00")?.cgColor
+        layer.lineWidth = 8
+        layer.lineDashPattern = [2, 2]
+        layer.fillColor = nil
+        return layer
+    }()
+    
     lazy var bgView: UIView = {
-        let bgView = UIView()
-        bgView.layer.cornerRadius = 4
-        bgView.layer.masksToBounds = true
-        bgView.backgroundColor = UIColor.init(hex: "#F1F1F3")
-        return bgView
+        let view = UIView()
+        view.layer.cornerRadius = 4
+        view.layer.masksToBounds = true
+        view.backgroundColor = UIColor(hex: "#F1F1F3")
+        return view
     }()
     
     lazy var leftLabel: UILabel = {
-        let leftLabel = UILabel()
-        leftLabel.textAlignment = .left
-        leftLabel.text = "Informasi yang sempurna"
-        leftLabel.textColor = UIColor.init(hex: "#9D9D9F")
-        leftLabel.font = UIFont.systemFont(ofSize: 12, weight: .regular)
-        return leftLabel
+        let label = UILabel()
+        label.text = "Informasi yang sempurna"
+        label.textColor = UIColor(hex: "#9D9D9F")
+        label.font = .systemFont(ofSize: 12)
+        return label
     }()
     
     lazy var rightLabel: UILabel = {
-        let rightLabel = UILabel()
-        rightLabel.textAlignment = .right
-        rightLabel.text = "Dapatkan kuota"
-        rightLabel.textColor = UIColor.init(hex: "#9D9D9F")
-        rightLabel.font = UIFont.systemFont(ofSize: 12, weight: .regular)
-        return rightLabel
+        let label = UILabel()
+        label.textAlignment = .right
+        label.text = "Dapatkan kuota"
+        label.textColor = UIColor(hex: "#9D9D9F")
+        label.font = .systemFont(ofSize: 12)
+        return label
     }()
     
     lazy var nameLabel: UILabel = {
-        let nameLabel = UILabel()
-        nameLabel.textAlignment = .center
-        nameLabel.textColor = UIColor.init(hex: "#333333")
-        nameLabel.font = UIFont.systemFont(ofSize: 10, weight: .regular)
-        nameLabel.layer.cornerRadius = 12
-        nameLabel.layer.masksToBounds = true
-        nameLabel.text = "0%\ntersisa"
-        nameLabel.numberOfLines = 0
-        nameLabel.backgroundColor = UIColor.init(hex: "#CDF300")
-        return nameLabel
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = UIColor(hex: "#333333")
+        label.font = .systemFont(ofSize: 10)
+        label.layer.cornerRadius = 13
+        label.layer.masksToBounds = true
+        label.numberOfLines = 0
+        label.backgroundColor = UIColor(hex: "#CDF300")
+        return label
     }()
     
     lazy var lineView: UIView = {
-        let lineView = UIView()
-        lineView.layer.cornerRadius = 4
-        lineView.layer.masksToBounds = true
-        lineView.backgroundColor = UIColor.init(hex: "#CDF300")
-        return lineView
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 4
+        view.layer.masksToBounds = true
+        return view
     }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(bgView)
-        addSubview(leftLabel)
-        addSubview(rightLabel)
-        addSubview(nameLabel)
-        addSubview(lineView)
-        bgView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(8)
-            make.width.equalTo(327)
-            make.left.equalToSuperview()
-            make.height.equalTo(8)
-        }
-        leftLabel.snp.makeConstraints { make in
-            make.left.equalToSuperview()
-            make.bottom.equalToSuperview()
-            make.height.equalTo(12)
-        }
-        rightLabel.snp.makeConstraints { make in
-            make.right.equalToSuperview()
-            make.bottom.equalToSuperview()
-            make.height.equalTo(12)
-        }
-        nameLabel.snp.makeConstraints { make in
-            make.centerY.equalTo(bgView)
-            make.left.equalToSuperview()
-            make.size.equalTo(CGSize(width: 62, height: 26))
-        }
-        lineView.snp.makeConstraints { make in
-            make.left.equalToSuperview()
-            make.top.bottom.equalTo(bgView)
-            make.right.equalTo(nameLabel.snp.left).offset(5)
-        }
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-
+    private func setupUI() {
+        addSubview(bgView)
+        addSubview(leftLabel)
+        addSubview(rightLabel)
+        addSubview(lineView)
+        addSubview(nameLabel)
+        
+        lineView.layer.addSublayer(dashLayer)
+        
+        bgView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(8)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(8)
+        }
+        
+        leftLabel.snp.makeConstraints { make in
+            make.left.bottom.equalToSuperview()
+        }
+        
+        rightLabel.snp.makeConstraints { make in
+            make.right.bottom.equalToSuperview()
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(bgView)
+            make.left.equalToSuperview().offset(0)
+            make.size.equalTo(CGSize(width: 62, height: 26))
+        }
+        
+        lineView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.top.bottom.equalTo(bgView)
+            make.right.equalTo(nameLabel.snp.centerX)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let path = CGMutablePath()
+        path.addLines(between: [
+            CGPoint(x: 0, y: lineView.bounds.midY),
+            CGPoint(x: lineView.bounds.width, y: lineView.bounds.midY)
+        ])
+        dashLayer.path = path
+        dashLayer.frame = lineView.bounds
+    }
+    
+    private func updateProgress(type: String) {
+        let width = self.frame.width > 0 ? self.frame.width : 327
+        var offset: CGFloat = 0
+        
+        switch type {
+        case "extreme1":
+            nameLabel.text = "0%\ntersisa"
+            offset = 0
+        case "extreme3":
+            nameLabel.text = "25%\ntersisa"
+            offset = width * 0.25 - 31
+        case "extreme4":
+            nameLabel.text = "50%\ntersisa"
+            offset = width * 0.5 - 31
+        case "extreme5":
+            nameLabel.text = "75%\ntersisa"
+            offset = width * 0.75 - 31
+        case "":
+            nameLabel.text = "100%\ntersisa"
+            offset = width - 62
+        default:
+            break
+        }
+        
+        nameLabel.snp.updateConstraints { make in
+            make.left.equalToSuperview().offset(max(0, offset))
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.layoutIfNeeded()
+        }
+    }
 }
